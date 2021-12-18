3d81268190c9b8200af62680f5f27ff7019e80b8
Vlasenkova <yfcnzvfcz@mail.ru>
2021-12-18 09:14:50 +0300

    вернулись к исходному значению с медицинской историей, сделала фрагмент с видео)

--- app/src/main/java/com/example/healther/MedicalHistoryActivity.kt
+++ app/src/main/java/com/example/healther/MedicalHistoryActivity.kt
@@ -2,7 +2,6 @@ package com.example.healther
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.example.healther.databinding.ActivityMedicalHistoryBinding
 
 class MedicalHistoryActivity : AppCompatActivity() {
@@ -10,44 +9,31 @@ class MedicalHistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMedicalHistoryBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
-        replaceFragment(MedHomeFragment.newInstance(), true)
-    }
+        val medHists = listOf(
+            MedicalHistory(
+                dataDay = 3,
+                dataMonth = "April",
+                text = "poseshenie 1"
+            ),
+            MedicalHistory(
+                dataDay = 8,
+                dataMonth = "May",
+                text = "poseshenie 2"
+            ),
+        )
+        val adapter = MedicalHistoriesAdapter()
+        binding.recyclerViewMedical.adapter = adapter
+        adapter.submitList(medHists)
 
-    fun replaceFragment(fragment: Fragment, istransition: Boolean) {
-        val fragmentTransition = supportFragmentManager.beginTransaction()
-        if (istransition) {
-            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,
-                android.R.anim.slide_in_left)
+        binding.buttonAdd.setOnClickListener {
+            adapter.addItem(
+                MedicalHistory(
+                    dataDay = 8,
+                    dataMonth = "June",
+                    text = "poseshenie 3"
+                )
+            )
         }
-        fragmentTransition.replace(R.id.frameLayout, fragment)
-            .addToBackStack(fragment.javaClass.simpleName)
     }
 }
-//        val medHists = listOf(
-//            MedicalHistory(
-//                dataDay = 3,
-//                dataMonth = "April",
-//                text = "poseshenie 1"
-//            ),
-//            MedicalHistory(
-//                dataDay = 8,
-//                dataMonth = "May",
-//                text = "poseshenie 2"
-//            ),
-//        )
-//        val adapter = MedicalHistoriesAdapter()
-//        binding.recyclerViewMedical.adapter = adapter
-//        adapter.submitList(medHists)
-//
-//        binding.buttonAdd.setOnClickListener {
-//            adapter.addItem(
-//                MedicalHistory(
-//                    dataDay = 8,
-//                    dataMonth = "June",
-//                    text = "poseshenie 3"
-//                )
-//            )
-//        }
-
