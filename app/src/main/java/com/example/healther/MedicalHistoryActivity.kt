b73e6fa9d11390c349dedcb9f6ee9c7bc31063a3
Vlasenkova <yfcnzvfcz@mail.ru>
2021-12-18 14:25:27 +0300

    закончила делать приложение.

--- app/src/main/java/com/example/healther/MedicalHistoryActivity.kt
+++ app/src/main/java/com/example/healther/MedicalHistoryActivity.kt
@@ -7,19 +7,23 @@ import com.example.healther.databinding.ActivityMedicalHistoryBinding
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+import java.util.*
 
 class MedicalHistoryActivity : AppCompatActivity() {
     lateinit var binding: ActivityMedicalHistoryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMedicalHistoryBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        binding.imageDietMenuBack.setOnClickListener {
+            finish()
+        }
         setContentView(binding.root)
         val dataTime = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             LocalDateTime.now()
         } else {
             TODO("VERSION.SDK_INT < O")
         }
-        val dataDay = dataTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL))
+        val dataDay = dataTime.format(DateTimeFormatter.ofPattern("dd, MMM", Locale.ENGLISH))
         val medHists = listOf(
             MedicalHistory(
                 dataDay = dataDay,
